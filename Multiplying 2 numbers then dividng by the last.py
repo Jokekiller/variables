7b01651548985cb2fcb87e31d59d90b49a881998
Harry <Harry@192.168.0.5>
2014-09-16 11:24:10 +0100

    Multiplying two numbers and dividng by the last

---
+++ Multiplying 2 numbers then dividng by the last.py
@@ -0,0 +1,12 @@
+#Harry Robinson
+#16-09-2014
+#Programme asking for three integers and multiplies two and divides by one
+
+print("This program will ask for three integers and multiply two of them and then divides by the third")
+number1 = int(input("Please give 1st number: "))
+number2 = int(input("Please give 2nd number: "))
+number3 = int(input("Please give 3rd number: "))
+answer = number1 * number2 / number3
+print("Answer is {0} * {1} / {2} = {3}".format(number1, number2, number3, answer))
+
+              
