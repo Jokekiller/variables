268c53e1c8bcdb88727c43204407261777ea3535
Harry <Harry@192.168.0.5>
2014-09-16 11:11:51 +0100

    Adding four numbers program

---
+++ Adding four numbers programme.py
@@ -0,0 +1,13 @@
+#Harry Robinson
+#16-09-2014
+#Adding four numbers
+
+print("This program will add fourintegers you give")
+number1 = int(input("Please give 1st number: "))
+number2 = int(input("Please give 2nd number: "))
+number3 = int(input("Please give 3rd number: "))
+number4 = int(input("Please give 4th number: "))
+answer = number1 + number2 + number3 + number4
+print("Answer is {0} + {1} + {2} + {3} = {4}".format(number1, number2, number3, number4, answer))
+
+              
