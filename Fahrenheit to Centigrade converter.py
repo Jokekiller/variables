3367b5c8a748127c0ec9daf2b9e13d9daa635250
Harry <Harry@192.168.0.5>
2014-09-18 12:24:45 +0100

    Fahrenheit to Centigrade converter

---
+++ Fahrenheit to Centigrade converter.py
@@ -0,0 +1,8 @@
+#Harry Robinson
+#18-09-2014
+#Fahrenheit to Centigrade converted
+
+print("This proramme will convert Fahrenheit to Centigrade:")
+fahrenheit = float(input("Give the Fahrenheit temperature:"))
+centigrade = (fahrenheit - 32)*(5/9)
+print("The temperature {0} in fahrenheit is {1} in centigrade".format(fahrenheit, centigrade))
