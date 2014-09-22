f83b664abdf4aebfa3b3d15d7c3716c2448b4e99
Harry Robinson <41194@longroad.ac.uk>
2014-09-22 12:16:58 +0100

    Converting height and weight

---
+++ Convertng someones height and weight.py
@@ -0,0 +1,8 @@
+#Harry Robinson
+#22/09/2014
+#Converting someones height and weight
+
+print("This prgramme will convert your height and weight")
+height = float(input("Give your height in inches; "))
+weight = float(input("Give your weight in stones; "))
+
