8145cc896e12f62578c601f25415708ad79b893b
Harry <Harry@192.168.0.8>
2014-09-25 10:14:10 +0100

    Programe caluclating volume of pool with different depths

---
+++ Program calculating the volume of a pool with different depths.py
@@ -0,0 +1,14 @@
+#Harry Robinson
+#25-09-2014
+#Programme calculating the volme of a pool with different depths
+
+print("This prgramme will calculate the volme of a pool with different depths")
+shallowEndDepth = float(input("Give the shallow end depth: "))
+deepEndDepth = float(input("Give the deep end depth:"))
+width = float(input("Give the width of the pool: "))
+length = float(input("Give the length of the pool: "))
+differenceInDepth = deepEndDepth - shallowEndDepth
+volumeOfSlopedBottom = (differenceInDepth * width * length)/2
+volumeOfPool = volumeOfSlopedBottom + (length * width * shallowEndDepth)
+volumeOfWater = volumeOfPool
+print("The amount of water required is {0}".format(volumeOfPool))
