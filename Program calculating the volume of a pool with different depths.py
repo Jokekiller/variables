5917a9b4c05e0c13a6d0a67e74a115a2ec3ad9e1
Harry Robinson <41194@longroad.ac.uk>
2014-09-26 08:59:49 +0100

    Different pool depths

---
+++ Program calculating the volume of a pool with different depths.py
@@ -0,0 +1,12 @@
+#Harry Robinson
+#23-09-2014
+#Program calculating the volume of a pool with different depths
+#Write a flow chart for this
+
+print("This programme will calculate the volume of a swimming pool with different depths")
+shallowEndDepth = float(input("Give the shallow end depth: "))
+poolLength = float(input("Give the length of the pool: "))
+poolWidth = float(input("Give the width of the pool: "))
+deepEndDepth = float(input("Give the deep end depth: "))
+volumeOfShallowEnd = shallowEndDepth * poolLength * poolWidth
+                    
